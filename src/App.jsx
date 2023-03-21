e3f042b3cdaf5e3fc46142b3fed21ef1ddef7400
VicScapini <victoria.scapini@gmail.com>
2023-03-21 13:48:10 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 
 import './App.css';
+import { Home } from "./Pages/Home/Home";
+import { Menu } from "./components/Menu/Menu";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Blog } from "./Pages/Blog/Blog";
+import { Contato } from "./Pages/Contato/Contato";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, React!</h1>
-     
+
+      <BrowserRouter>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="/Blog" element={<Blog />} />
+          <Route path="/contato" element={<Contato />} />
+        </Routes>
+      </BrowserRouter>
+
+
+
+
     </div>
   );
 }
---
+++ src/Pages/Contato/Contato.jsx
@@ -0,0 +1,7 @@
+export function Contato () {
+    return (
+        <div classname="contato">
+            <h1>Contato</h1>
+        </div>
+    );
+}
